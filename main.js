b7aa02174f11d0b914784aa85721ee7f57dae2ee
Gaven Dcosta <gavendcosta@gmail.com>
2023-09-02 21:09:21 +0530

    icons

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// import { addIcons } from "./js/fontAwesome";
+import { addIcons } from "./js/fontAwesome";
 import { addParallax } from "./js/parallax";
 import { renderTeamCards } from "./js/team";
 import { tiltLogic } from "./js/tilt";
 
-// addIcons()
-// addParallax()
+addIcons()
+//addParallax()
 tiltLogic()
 renderTeamCards()
