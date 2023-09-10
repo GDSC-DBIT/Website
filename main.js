bdada1952d791c467a2570dae268520cbfb4078a
Gaven Dcosta <gavendcosta@gmail.com>
2023-09-10 13:22:39 +0530

    nav menu change

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ import { addIcons } from "./js/fontAwesome";
 import { addParallax } from "./js/parallax";
 import { renderTeamCards } from "./js/team";
 import { tiltLogic } from "./js/tilt";
+import { toggleMenu } from "./js/navbar";
 
 addIcons()
 addParallax()
 tiltLogic()
 renderTeamCards()
 handleForm()
+toggleMenu()
